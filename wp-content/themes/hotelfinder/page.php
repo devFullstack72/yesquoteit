b98482a46db961e09012e42ba8157635746a0df4
Rehan <rehanmasiya786@gmail.com>
2025-02-06 19:47:20 +0530

    button scroll to lead

--- wp-content/themes/hotelfinder/page.php
+++ wp-content/themes/hotelfinder/page.php
@@ -1,13 +1,13 @@
-<?php
-
-get_header();
-
-if ( have_posts() ) {
+<?php get_header();?>
+<div class="container">
+	<?php 
+	if ( have_posts() ) {
 				while ( have_posts() ) {
 					the_post();
 					the_content();
 					
 				}
 			}
-?>
+	 ?>
+</div>
 <?php get_footer(); ?>
