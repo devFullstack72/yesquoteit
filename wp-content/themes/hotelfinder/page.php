f67686af1a34f9ecb8f15eeca5b337a1a1d5c5b1
Rehan <rehanmasiya786@gmail.com>
2025-02-11 14:21:38 +0530

    design and logo changes

--- wp-content/themes/hotelfinder/page.php
+++ wp-content/themes/hotelfinder/page.php
@@ -1,13 +1,19 @@
-<?php get_header();?>
-<div class="container">
-	<?php 
-	if ( have_posts() ) {
-				while ( have_posts() ) {
-					the_post();
-					the_content();
-					
-				}
-			}
-	 ?>
+<?php get_header(); ?>
+
+<?php 
+    // Get the current page slug
+    $page_slug = get_post_field( 'post_name', get_the_ID() );
+?>
+
+<div class="container <?php echo ($page_slug == 'about-us') ? 'about-us-page' : ''; ?>">
+    <?php 
+        if ( have_posts() ) {
+            while ( have_posts() ) {
+                the_post();
+                the_content();
+            }
+        }
+    ?>
 </div>
+
 <?php get_footer(); ?>
