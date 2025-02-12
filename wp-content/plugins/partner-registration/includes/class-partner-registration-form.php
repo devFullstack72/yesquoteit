e804efa32e532a2f96999c54505b11f7efde89fd
mahamadali <manknojiya121@gmail.com>
2025-02-12 18:16:53 +0530

    continue development...

--- wp-content/plugins/partner-registration/includes/class-partner-registration-form.php
+++ wp-content/plugins/partner-registration/includes/class-partner-registration-form.php
@@ -25,7 +25,7 @@ class Partner_Registration_Form
             SELECT ID, post_title 
             FROM {$wpdb->posts} 
             WHERE post_type = 'lead_generation' 
-            AND post_status != 'trash'
+            AND post_status == 'publish'
         ");
 
         ob_start();
