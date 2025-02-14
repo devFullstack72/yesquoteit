eed1a1f2b0b0c09bc0ceca95bfd0b47a93c091bc
mahamadali <manknojiya121@gmail.com>
2025-02-14 12:08:34 +0530

    continue development...

--- wp-content/plugins/partner-registration/includes/class-partner-admin.php
+++ wp-content/plugins/partner-registration/includes/class-partner-admin.php
@@ -10,7 +10,26 @@ class Partner_Admin
     {
         add_action('admin_menu', [$this, 'register_admin_menu']);
         add_action('admin_init', [$this, 'handle_partner_actions']);
+        add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_scripts']);
     }
+
+    public function enqueue_admin_scripts($hook)
+    {
+        // Load scripts only on the specific admin page
+        if ($hook !== 'toplevel_page_service-partners') {
+            return;
+        }
+
+        // Ensure jQuery is included
+        wp_enqueue_script('jquery');
+
+        // Enqueue Google Maps API
+        wp_enqueue_script('google-maps-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDuoh4RV3jwuAD72LBq02e3rx4-iZa-wLc&libraries=places', [], null, true);
+
+        // Custom script for autocomplete
+        wp_enqueue_script('partner-registration-script', plugin_dir_url(__FILE__) . 'js/partner-registration.js', ['jquery', 'google-maps-api'], null, true);
+    }
+
 
     public function register_admin_menu()
     {
@@ -110,15 +129,41 @@ class Partner_Admin
             $email = sanitize_email($_POST['email']);
             $phone = sanitize_text_field($_POST['phone']);
             $address = sanitize_text_field($_POST['address']);
+            $latitude = sanitize_text_field($_POST['latitude']);
+            $longitude = sanitize_text_field($_POST['longitude']);
+            $street_number = sanitize_text_field($_POST['street_number']);
+            $route = sanitize_text_field($_POST['route']);
+            $address2 = sanitize_text_field($_POST['address2']);
+            $postal_code = sanitize_text_field($_POST['postal_code']);
+            $state = sanitize_text_field($_POST['state']);
+            $country = sanitize_text_field($_POST['country']);
+            $service_area = sanitize_text_field($_POST['service_area']);
+            $other_country = sanitize_text_field($_POST['other_country']);
             $status = intval($_POST['status']);
             $selected_leads = isset($_POST['leads']) ? array_map('intval', $_POST['leads']) : [];
         
             // Update partner details
             $wpdb->update(
                 $wpdb->prefix . 'service_partners',
-                ['name' => $name, 'email' => $email, 'phone' => $phone, 'address' => $address, 'status' => $status],
+                [
+                    'name' => $name,
+                    'email' => $email,
+                    'phone' => $phone,
+                    'address' => $address,
+                    'latitude' => $latitude,
+                    'longitude' => $longitude,
+                    'street_number' => $street_number,
+                    'route' => $route,
+                    'address2' => $address2,
+                    'postal_code' => $postal_code,
+                    'state' => $state,
+                    'country' => $country,
+                    'service_area' => $service_area,
+                    'other_country' => $other_country,
+                    'status' => $status
+                ],
                 ['id' => $partner_id],
-                ['%s', '%s', '%s', '%s', '%d'],
+                ['%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%d'],
                 ['%d']
             );
         
@@ -198,6 +243,12 @@ class Partner_Admin
         global $wpdb;
         $wp_posts_table = $wpdb->prefix . 'posts';
         $lead_partners_table = $wpdb->prefix . 'lead_partners';
+        $countries_table = $wpdb->prefix . 'countries';
+        
+        $countries = $wpdb->get_results("
+            SELECT * 
+            FROM {$countries_table}
+        ");
     
         // Get all leads (Post type: 'lead_generation')
         $all_leads = $wpdb->get_results("SELECT ID, post_title FROM $wp_posts_table WHERE post_type = 'lead_generation'");
@@ -223,10 +274,80 @@ class Partner_Admin
                         <th><label for="phone">Phone</label></th>
                         <td><input type="text" name="phone" id="phone" value="<?php echo esc_attr($partner->phone); ?>" class="regular-text"></td>
                     </tr>
-                    <tr>
+                    <!-- <tr>
                         <th><label for="address">Address</label></th>
                         <td><input type="text" name="address" id="address" value="<?php echo esc_attr($partner->address); ?>" class="regular-text"></td>
+                    </tr> -->
+                    <tr>
+                        <th><label for="address">Address</label></th>
+                        <td>
+                            <input type="text" id="autocomplete" class="regular-text" aria-required="true" aria-invalid="false" name="address" value="<?php echo esc_attr($partner->address); ?>">
+                        </td>
                     </tr>
+                    <tr>
+                        <th><label>Latitude</label></th>
+                        <td><input type="text" id="latitude" name="latitude" value="<?php echo esc_attr($partner->latitude); ?>" class="regular-text" readonly></td>
+                    </tr>
+                    <tr>
+                        <th><label>Longitude</label></th>
+                        <td><input type="text" id="longitude" name="longitude" value="<?php echo esc_attr($partner->longitude); ?>" class="regular-text" readonly></td>
+                    </tr>
+                    <tr>
+                        <th><label>Street Number</label></th>
+                        <td><input type="text" id="street_number" name="street_number" value="<?php echo esc_attr($partner->street_number); ?>" class="regular-text" readonly></td>
+                    </tr>
+                    <tr>
+                        <th><label>Address 1</label></th>
+                        <td><input type="text" id="route" name="route" value="<?php echo esc_attr($partner->route); ?>" class="regular-text" readonly></td>
+                    </tr>
+                    <tr>
+                        <th><label>Address 2</label></th>
+                        <td><input type="text" id="address2" name="address2" value="<?php echo esc_attr($partner->address2); ?>" class="regular-text" readonly></td>
+                    </tr>
+                    <tr>
+                        <th><label>Postal Code</label></th>
+                        <td><input type="text" id="postal_code" name="postal_code" value="<?php echo esc_attr($partner->postal_code); ?>" class="regular-text" readonly></td>
+                    </tr>
+                    <tr>
+                        <th><label>State</label></th>
+                        <td><input type="text" id="state" name="state" value="<?php echo esc_attr($partner->state); ?>" class="regular-text" readonly></td>
+                    </tr>
+                    <tr>
+                        <th><label>Country</label></th>
+                        <td><input type="text" id="country" name="country" value="<?php echo esc_attr($partner->country); ?>" class="regular-text" readonly></td>
+                    </tr>
+                    <tr>
+                        <th><label>Service Area</label></th>
+                        <td>
+                            <select name="service_area" id="radius" class="cls_slect-radius" onchange="on_country()">
+                                <option value="5" <?php echo ($partner->service_area == "5") ? 'selected' : ''; ?>> 5 KM </option>
+                                <option value="10" <?php echo ($partner->service_area == "10") ? 'selected' : ''; ?>> 10 KM </option>
+                                <option value="25" <?php echo ($partner->service_area == "25") ? 'selected' : ''; ?>> 25 KM </option>
+                                <option value="50" <?php echo ($partner->service_area == "50") ? 'selected' : ''; ?>> 50 KM </option>
+                                <option value="100" <?php echo ($partner->service_area == "100") ? 'selected' : ''; ?>> 100 KM </option>
+                                <option value="250" <?php echo ($partner->service_area == "250") ? 'selected' : ''; ?>> 250 KM </option>
+                                <option value="500" <?php echo ($partner->service_area == "500") ? 'selected' : ''; ?>> 500 KM </option>
+                                <option value="entire" <?php echo ($partner->service_area == "entire") ? 'selected' : ''; ?>> Entire Country </option>
+                                <option value="state" <?php echo ($partner->service_area == "state") ? 'selected' : ''; ?>> Entire State </option>
+                                <option value="other" <?php echo ($partner->service_area == "other") ? 'selected' : ''; ?>> Other Country </option>
+                                <option value="every" <?php echo ($partner->service_area == "every") ? 'selected' : ''; ?>> Every Where </option>
+                                <option value="no_service" <?php echo ($partner->service_area == "no_service") ? 'selected' : ''; ?>> Not at this location </option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr id="show_country">
+                        <th><label>Service provided in other Country</label></th>
+                        <td>
+                            <select name="other_country" id="other_country" class="cls_slect-radius">
+                                <?php foreach($countries as $country) { ?>
+                                    <option value="<?php echo $country->code; ?>" <?php echo ($partner->other_country == $country->code) ? 'selected' : ''; ?>>
+                                        <?php echo esc_html($country->name); ?>
+                                    </option>
+                                <?php } ?>
+                            </select>
+                        </td>
+                    </tr>
+
                     <tr>
                         <th><label for="status">Status</label></th>
                         <td>
@@ -237,18 +358,19 @@ class Partner_Admin
                             </select>
                         </td>
                     </tr>
+
                     <tr>
-                    <th><label>Assign Leads</label></th>
-                    <td>
-                        <?php foreach ($all_leads as $lead): ?>
-                            <label>
-                                <input type="checkbox" name="leads[]" value="<?php echo $lead->ID; ?>" 
-                                    <?php checked(in_array($lead->ID, $assigned_leads)); ?>>
-                                <?php echo esc_html($lead->post_title); ?>
-                            </label><br>
-                        <?php endforeach; ?>
-                    </td>
-                </tr>
+                        <th><label>Assign Leads</label></th>
+                        <td>
+                            <?php foreach ($all_leads as $lead): ?>
+                                <label>
+                                    <input type="checkbox" name="leads[]" value="<?php echo $lead->ID; ?>" 
+                                        <?php checked(in_array($lead->ID, $assigned_leads)); ?>>
+                                    <?php echo esc_html($lead->post_title); ?>
+                                </label><br>
+                            <?php endforeach; ?>
+                        </td>
+                    </tr>
                 </table>
                 <p><input type="submit" name="update_partner" value="Save Changes" class="button button-primary"></p>
             </form>
--- wp-content/plugins/partner-registration/partner-registration.php
+++ wp-content/plugins/partner-registration/partner-registration.php
@@ -36,6 +36,7 @@ class Partner_Registration_Plugin {
 
         $service_partners_table = $wpdb->prefix . 'service_partners';
         $lead_partners_table = $wpdb->prefix . 'lead_partners';
+        $countries_table = $wpdb->prefix . 'countries';
 
         $sql_service_partners = "CREATE TABLE $service_partners_table (
             id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -64,8 +65,64 @@ class Partner_Registration_Plugin {
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         ) $charset_collate;";
 
+        $sql_countries = "CREATE TABLE $countries_table (
+            id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            code VARCHAR(10) NOT NULL UNIQUE,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) $charset_collate;";
+
         dbDelta($sql_service_partners);
         dbDelta($sql_lead_partners);
+        dbDelta($sql_countries);
+
+        // Insert countries data if the table is empty
+        $existing_countries = $wpdb->get_var("SELECT COUNT(*) FROM $countries_table");
+
+        if ($existing_countries == 0) {
+            $countries = [
+                ['Afghanistan', 'AF'], ['Albania', 'AL'], ['Algeria', 'DZ'], ['Andorra', 'AD'], 
+                ['Angola', 'AO'], ['Argentina', 'AR'], ['Armenia', 'AM'], ['Australia', 'AU'],
+                ['Austria', 'AT'], ['Azerbaijan', 'AZ'], ['Bahamas', 'BS'], ['Bahrain', 'BH'],
+                ['Bangladesh', 'BD'], ['Barbados', 'BB'], ['Belarus', 'BY'], ['Belgium', 'BE'],
+                ['Belize', 'BZ'], ['Benin', 'BJ'], ['Bhutan', 'BT'], ['Bolivia', 'BO'],
+                ['Bosnia and Herzegovina', 'BA'], ['Botswana', 'BW'], ['Brazil', 'BR'], 
+                ['Brunei', 'BN'], ['Bulgaria', 'BG'], ['Burkina Faso', 'BF'], ['Burundi', 'BI'],
+                ['Cambodia', 'KH'], ['Cameroon', 'CM'], ['Canada', 'CA'], ['Chad', 'TD'], 
+                ['Chile', 'CL'], ['China', 'CN'], ['Colombia', 'CO'], ['Congo', 'CG'],
+                ['Costa Rica', 'CR'], ['Croatia', 'HR'], ['Cuba', 'CU'], ['Cyprus', 'CY'],
+                ['Czech Republic', 'CZ'], ['Denmark', 'DK'], ['Dominican Republic', 'DO'],
+                ['Ecuador', 'EC'], ['Egypt', 'EG'], ['El Salvador', 'SV'], ['Estonia', 'EE'],
+                ['Ethiopia', 'ET'], ['Finland', 'FI'], ['France', 'FR'], ['Gabon', 'GA'],
+                ['Germany', 'DE'], ['Ghana', 'GH'], ['Greece', 'GR'], ['Guatemala', 'GT'],
+                ['Honduras', 'HN'], ['Hungary', 'HU'], ['Iceland', 'IS'], ['India', 'IN'],
+                ['Indonesia', 'ID'], ['Iran', 'IR'], ['Iraq', 'IQ'], ['Ireland', 'IE'],
+                ['Israel', 'IL'], ['Italy', 'IT'], ['Jamaica', 'JM'], ['Japan', 'JP'],
+                ['Jordan', 'JO'], ['Kazakhstan', 'KZ'], ['Kenya', 'KE'], ['Kuwait', 'KW'],
+                ['Latvia', 'LV'], ['Lebanon', 'LB'], ['Libya', 'LY'], ['Lithuania', 'LT'],
+                ['Luxembourg', 'LU'], ['Malaysia', 'MY'], ['Maldives', 'MV'], ['Mexico', 'MX'],
+                ['Moldova', 'MD'], ['Monaco', 'MC'], ['Mongolia', 'MN'], ['Morocco', 'MA'],
+                ['Myanmar', 'MM'], ['Nepal', 'NP'], ['Netherlands', 'NL'], ['New Zealand', 'NZ'],
+                ['Nigeria', 'NG'], ['North Korea', 'KP'], ['Norway', 'NO'], ['Oman', 'OM'],
+                ['Pakistan', 'PK'], ['Palestine', 'PS'], ['Panama', 'PA'], ['Peru', 'PE'],
+                ['Philippines', 'PH'], ['Poland', 'PL'], ['Portugal', 'PT'], ['Qatar', 'QA'],
+                ['Romania', 'RO'], ['Russia', 'RU'], ['Saudi Arabia', 'SA'], ['Serbia', 'RS'],
+                ['Singapore', 'SG'], ['Slovakia', 'SK'], ['Slovenia', 'SI'], ['South Africa', 'ZA'],
+                ['South Korea', 'KR'], ['Spain', 'ES'], ['Sri Lanka', 'LK'], ['Sudan', 'SD'],
+                ['Sweden', 'SE'], ['Switzerland', 'CH'], ['Syria', 'SY'], ['Taiwan', 'TW'],
+                ['Tanzania', 'TZ'], ['Thailand', 'TH'], ['Tunisia', 'TN'], ['Turkey', 'TR'],
+                ['Ukraine', 'UA'], ['United Arab Emirates', 'AE'], ['United Kingdom', 'GB'],
+                ['United States', 'US'], ['Venezuela', 'VE'], ['Vietnam', 'VN'], ['Yemen', 'YE'],
+                ['Zambia', 'ZM'], ['Zimbabwe', 'ZW']
+            ];
+
+            foreach ($countries as $country) {
+                $wpdb->insert($countries_table, [
+                    'name' => $country[0],
+                    'code' => $country[1]
+                ]);
+            }
+        }
     }
 }
 
