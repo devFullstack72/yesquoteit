d6970586a5034bcf7b9bdc116c9aa22c970c0957
Mahammadali <manknojiya121@gmail.com>
2025-02-19 11:08:50 +0530

    Add a go button

--- wp-content/themes/hotelfinder/front-page.php
+++ wp-content/themes/hotelfinder/front-page.php
@@ -71,7 +71,7 @@
                         <span id="category-error" class="error-message" style="color: red; display: none;">Please select a category or enter a search lead text.</span>
                     </div>
 
-                    <div id="htlfndr-input-5" style="margin:1px;margin-right: 10px;">
+                    <div id="htlfndr-input-5" style="margin:1px;margin-right: 10px; width: 50px !important;">
                         <input type="submit" value="Go" />
                     </div>
 
@@ -79,7 +79,7 @@
                         <input type="text" name="htlfndr-search" id="htlfndr-search" class="search-hotel-input" placeholder="Search by lead name" />
                     </div>
 
-                    <div id="htlfndr-input-5" style="margin:1px;">
+                    <div id="htlfndr-input-5" style="margin:1px; width: 120px !important;">
                         <input type="submit" value="Search" />
                     </div>
                 </form>
