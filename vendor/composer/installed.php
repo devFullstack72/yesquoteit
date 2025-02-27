7dfd8eaa82ef81024de94fa4f49c11a437d31cb2
mahamadali <manknojiya121@gmail.com>
2025-02-27 13:57:20 +0530

    twillio sms...

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd7d5dfe725da17d17495a9d1de299fb9cfdbddaa',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd7d5dfe725da17d17495a9d1de299fb9cfdbddaa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '8.3.15',
+            'version' => '8.3.15.0',
+            'reference' => '3113e8a672edf5e012120fa6fafc201e013c7ba4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
